b479aff28c4dbaa3c77c7724e6934ac7eaf917ba
Robert <adrianrobert288@gmail.com>
2023-04-02 21:13:38 -0300

    Criado classe QuestionBank
    # Feito algumas alterações no layout
    # Criado um constructor e um getter para QuestionList
    # Iniciado a lógica do QuizActivity

---
+++ app/src/main/java/com/hilab/quiz/QuestionBank.java
@@ -0,0 +1,72 @@
+package com.hilab.quiz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionBank {
+    private static List<QuestionsList> easyQuestions() {
+
+        final List<QuestionsList> questionsLists = new ArrayList<>();
+
+        final QuestionsList question1 = new QuestionsList("Qual é a capital do Brasil?", "São Paulo", "Brasília", "Rio de Janeiro", "Salvador", "Brasília", "");
+        final QuestionsList question2 = new QuestionsList("Qual é o maior continente do mundo?", "África", "Europa", "Ásia", "América do Norte", "Ásia", "");
+        final QuestionsList question3 = new QuestionsList("Qual é o maior planeta do sistema solar?", "Terra", "Júpiter", "Vênus", "Marte", "Júpiter", "");
+        final QuestionsList question4 = new QuestionsList("Qual é o país que tem a maior população do mundo?", "Índia", "Rússia", "China", "Estados Unidos", "China", "");
+        final QuestionsList question5 = new QuestionsList("Qual é o maior oceano do mundo?", "Atlântico", "Pacífico", "Índico", "Ártico", "Pacífico", "");
+        final QuestionsList question6 = new QuestionsList("Qual é o nome do famoso monumento na cidade do Rio de Janeiro?", "Torre Eiffel", "Estátua da Liberdade", "Cristo Redentor", "Coliseu", "Cristo Redentor", "");
+        final QuestionsList question7 = new QuestionsList("Qual é o nome do criador do Facebook?", "Bill Gates", "Steve Jobs", "Mark Zuckerberg", "Jeff Bezos", "Mark Zuckerberg", "");
+        final QuestionsList question8 = new QuestionsList("Qual é a montanha mais alta do mundo?", "Monte Kilimanjaro", "Montanha do Everest", "Montanha Denali", "Monte Aconcágua", "Montanha Everest", "");
+        final QuestionsList question9 = new QuestionsList("Qual é a capital da Argentina?", "Santiago", "Buenos Aires", "Lima", "Bogotá", "Buenos Aires", "");
+        final QuestionsList question10 = new QuestionsList("Qual é o nome da galáxia em que o nosso sistema solar está localizado?", "Via Láctea", "Andrômeda", "Nebulosa do Caranguejo", "Estrela Polar", "Via Láctea", "");
+
+        questionsLists.add(question1);
+        questionsLists.add(question2);
+        questionsLists.add(question3);
+        questionsLists.add(question4);
+        questionsLists.add(question5);
+        questionsLists.add(question6);
+        questionsLists.add(question7);
+        questionsLists.add(question8);
+        questionsLists.add(question9);
+        questionsLists.add(question10);
+
+        return questionsLists;
+    }
+
+    private static List<QuestionsList> hardQuestions() {
+        final List<QuestionsList> questionsLists = new ArrayList<>();
+
+        final QuestionsList question1 = new QuestionsList("Qual é o nome da maior cordilheira do mundo?", "Cordilheira dos Andes", "Cordilheira do Himalaia", "Cordilheira dos Alpes", "Cordilheira dos Apeninos", "Cordilheira do Himalaia", "");
+        final QuestionsList question2 = new QuestionsList("Qual é a data exata em que a Guerra Fria terminou?", "9 de novembro de 1989", "25 de dezembro de 1991", "8 de agosto de 1988", "12 de junho de 1990", "25 de dezembro de 1991", "");
+        final QuestionsList question3 = new QuestionsList("Qual é o nome do fenômeno meteorológico que provoca a diminuição da temperatura na superfície terrestre?", "El Niño", "La Niña", "Chuva ácida", "Inversão térmica", "Inversão térmica", "");
+        final QuestionsList question4 = new QuestionsList("Qual é o nome da primeira pessoa a atingir o pico do Monte Everest?", "Tenzing Norgay", "Reinhold Messner", "Sir Edmund Hillary", "Maurice Herzog", "Sir Edmund Hillary", "");
+        final QuestionsList question5 = new QuestionsList("Qual é o nome do elemento químico cujo símbolo é Au?", "Prata", "Ouro", "Cobre", "Ferro", "Ouro", "");
+        final QuestionsList question6 = new QuestionsList("Qual é o nome do escritor brasileiro que escreveu o livro 'Memórias Póstumas de Brás Cubas'?", "Machado de Assis", "Graciliano Ramos", "Clarice Lispector", "Guimarães Rosa", "Machado de Assis", "");
+        final QuestionsList question7 = new QuestionsList("Qual é o nome do líder soviético que instaurou a política de 'glasnost' e 'perestroika'?", "Mikhail Gorbachev", "Joseph Stalin", "Leonid Brezhnev", "Vladimir Putin", "Mikhail Gorbachev", "");
+        final QuestionsList question8 = new QuestionsList("Qual é o nome do físico alemão que formulou a Teoria da Relatividade?", "Albert Einstein", "Isaac Newton", "Niels Bohr", "Galileu Galilei", "Albert Einstein", "");
+        final QuestionsList question9 = new QuestionsList("Qual é o nome da cidade italiana conhecida como a 'cidade eterna'?", "Florença", "Milão", "Veneza", "Roma", "Roma", "");
+        final QuestionsList question10 = new QuestionsList("Qual é o nome do filósofo grego que foi discípulo de Platão e mestre de Alexandre, o Grande?", "Aristóteles", "Sócrates", "Demócrito", "Epicuro", "Aristóteles", "");
+
+        questionsLists.add(question1);
+        questionsLists.add(question2);
+        questionsLists.add(question3);
+        questionsLists.add(question4);
+        questionsLists.add(question5);
+        questionsLists.add(question6);
+        questionsLists.add(question7);
+        questionsLists.add(question8);
+        questionsLists.add(question9);
+        questionsLists.add(question10);
+
+        return questionsLists;
+    }
+
+    private static List<QuestionsList> getQuestion(String selectedTopicName) {
+        switch (selectedTopicName) {
+            case "Fácil":
+                return easyQuestions();
+            default:
+                return hardQuestions();
+        }
+    }
+}
--- app/src/main/java/com/hilab/quiz/QuizActivity.java
+++ app/src/main/java/com/hilab/quiz/QuizActivity.java
@@ -1,12 +1,37 @@
 package com.hilab.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private TextView questions;
+    private TextView question;
+
+    private AppCompatButton option1, option2, option3, option4;
+
+    private AppCompatButton nextBtn;
+
+    private Timer quizTimer;
+
+    private int totalTimerInMin = 1;
+    private int seconds = 0;
+
+    private final List<QuestionsList> questionsList = ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +42,163 @@ public class QuizActivity extends AppCompatActivity {
         final TextView timer = findViewById(R.id.timer);
         final TextView topicName = findViewById(R.id.topicName);
 
+        questions = findViewById(R.id.qtyQuestion);
+        question = findViewById(R.id.showQuestions);
+
+        option1 = findViewById(R.id.option1);
+        option2 = findViewById(R.id.option1);
+        option3 = findViewById(R.id.option1);
+        option4 = findViewById(R.id.option1);
+
+        nextBtn = findViewById(R.id.nextBtn);
+
         final String getSelectedTopicName = getIntent().getStringExtra("selectedTopic");
 
         topicName.setText(getSelectedTopicName);
+
+        startTimer(timer);
+
+        option1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        option2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        option3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        option4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                quizTimer.purge();
+                quizTimer.cancel();
+
+                startActivity(new Intent(QuizActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void startTimer(TextView timerTextview){
+            quizTimer = new Timer();
+
+            quizTimer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+
+                    if(seconds == 0) {
+                        totalTimerInMin++;
+                        seconds = 59;
+                    }
+                    else if(seconds == 0 && totalTimerInMin == 0) {
+                        quizTimer.purge();
+                        quizTimer.cancel();
+
+                        Toast.makeText(QuizActivity.this, "Acabou o tempo", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(QuizActivity.this, QuizResults.class);
+                        intent.putExtra("acertos", getCorrectAnswer());
+                        intent.putExtra("erros", getIncorrectAnswer());
+                        startActivity(intent);
+
+                        finish();
+                    }
+                    else {
+                        seconds--;
+                    }
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+
+                            String finalMinutes = String.valueOf(totalTimerInMin);
+                            String finalSeconds = String.valueOf(seconds);
+
+                            if(finalMinutes.length() == 1) {
+                                finalMinutes = "0" + finalMinutes;
+                            }
+
+                            if(finalSeconds.length() == 1) {
+                                finalSeconds = "0" + finalSeconds;
+                            }
+
+                            timerTextview.setText(finalMinutes+":"+finalSeconds);
+                        }
+                    });
+                }
+            }, 1000, 1000);
+    }
+
+    private int getCorrectAnswer() {
+
+        int correctAnswer = 0;
+
+        for(int i=0;i < questionsList.size();i++){
+
+            final String getUserSelectedAnswer = questionsList.get(i).getUserSelectedAnswer();
+            final String getAnswer = questionsList.get(i).getAnswer();
+
+            if(getUserSelectedAnswer.equals(getAnswer)) {
+                correctAnswer++;
+            }
+
+        }
+
+        return correctAnswer;
+    }
+
+    private int getIncorrectAnswer() {
+
+        int correctAnswer = 0;
+
+        for(int i=0;i < questionsList.size();i++){
+
+            final String getUserSelectedAnswer = questionsList.get(i).getUserSelectedAnswer();
+            final String getAnswer = questionsList.get(i).getAnswer();
+
+            if(!getUserSelectedAnswer.equals(getAnswer)) {
+                correctAnswer++;
+            }
+
+        }
+
+        return correctAnswer;
+    }
+
+    @Override
+    public void onBackPressed() {
+        quizTimer.purge();
+        quizTimer.cancel();
+
+        startActivity(new Intent(QuizActivity.this, MainActivity.class));
+        finish();
     }
 }
